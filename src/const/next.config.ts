6c725663f9ccb8b20d6bbb7006b5882478ff2333
119003 <it.da.fukase@iftc.co.jp>
2021-08-25 10:47:30 +0900

    修正

--- src/const/next.config.ts
+++ src/const/next.config.ts
@@ -4,8 +4,8 @@ export namespace SystemConst {
     /** サーバー */
     export namespace Server {
         /** ホスト名 */
-        export const AP_HOST = 'http://localhost:8080/';
-        // export const AP_HOST = "https://boardgameap.herokuapp.com/";
+        //export const AP_HOST = 'http://localhost:8080/';
+        export const AP_HOST = 'https://boardgameap.herokuapp.com/';
         export const CREATE_ROOM = 'createroom';
         export const ENDPOINT = 'boardgame-endpoint';
         export const SITE_URL = 'https://board-game-three.vercel.app';
